9ee9c7a4ac8b24107f058f2468dfa74d0f51f78f
fotis tsakiris <footios76@gmail.com>
2020-02-23 04:38:45 +0200

    2) DeckSwiper: Image move

--- README.md
+++ README.md
@@ -3,8 +3,16 @@ A Youtube Tutorial:
 [React Native Animation Series](https://www.youtube.com/watch?v=LP6zmnMcHR0&list=PLy9JCsy2u97k6olfalMTA_XSPz4pNuT46)
 
 
-### TwitterScrollable
-
+1. TwitterScrollable
 - [Animated.event](https://animationbook.codedaily.io/animated-event/)
 - [interpolation](https://facebook.github.io/react-native/docs/animations#interpolation)
 - [How does Animated.Event work in React Native?](https://stackoverflow.com/questions/43510145/how-does-animated-event-work-in-react-native)
+
+
+2. DeckSwiper
+- [PanResponder](https://facebook.github.io/react-native/docs/panresponder)
+- [getLayout](https://animationbook.codedaily.io/get-layout/)
+- `getLayout` is a method of [AnimatedValueXY](https://facebook.github.io/react-native/docs/animatedvaluexy#getlayout)
+- [Tracking gestures](https://facebook.github.io/react-native/docs/animations#tracking-gestures)
+- [A gestureState object has the following](https://facebook.github.io/react-native/docs/panresponder#__docusaurus)
+    
--- src/DeckSwiper.js
+++ src/DeckSwiper.js
@@ -1,5 +1,13 @@
 import React, { Component } from "react";
-import { View, Text, Image, StyleSheet, Dimensions } from "react-native";
+import {
+  View,
+  Text,
+  Image,
+  StyleSheet,
+  Dimensions,
+  Animated,
+  PanResponder
+} from "react-native";
 
 const SCREEN_HEIGHT = Dimensions.get("window").height;
 const SCREEN_WIDTH = Dimensions.get("window").width;
@@ -13,49 +21,82 @@ const ARTICLES = [
 ];
 
 class DeckSwiper extends Component {
+  constructor(props) {
+    super(props);
+    this.position = new Animated.ValueXY();
+    this.state = { currenctIndex: 0 };
+  }
+
+  //   console.log('position', position);
+
+  UNSAFE_componentWillMount() {
+    this.PanResponder = PanResponder.create({
+        onStartShouldSetPanResponder: (e, gestureState) => true,
+        onPanResponderMove: (evt, gestureState) => {
+        // update the position when user starts panning
+        // to the new position
+        // `dy` because we only want to move vertically.
+        this.position.setValue({x: 0, y: gestureState.dy });
+      },
+      onPanResponderRelease: (evt, gestureState) => {
+        //
+      }
+    });
+  }
   renderArticles = () => {
     return ARTICLES.map((item, i) => {
       return (
-        <View
+        <Animated.View
           key={item.id}
-          style={{
-            flex: 1,
-            position: "absolute", // to get the text only one time
-            height: SCREEN_HEIGHT,
-            width: SCREEN_WIDTH,
-            backgroundColor: "white"
-          }}
+          style={this.position.getLayout()}
+          {...this.PanResponder.panHandlers}
         >
-          <View style={{ flex: 2, backgroundColor: 'black' }}>
-          <Image source={ARTICLES[i].uri} style={{flex: 1, height: null, width: null, resizeMode: 'center'}} />
-          </View>
-          <View style={{ flex: 3, padding: 5 }}>
+          <View
+            style={{
+              flex: 1,
+              position: "absolute", // to get the text only one time
+              height: SCREEN_HEIGHT,
+              width: SCREEN_WIDTH,
+              backgroundColor: "white"
+            }}
+          >
+            <View style={{ flex: 2, backgroundColor: "black" }}>
+              <Image
+                source={ARTICLES[i].uri}
+                style={{
+                  flex: 1,
+                  height: null,
+                  width: null,
+                  resizeMode: "center"
+                }}
+              />
+            </View>
+            <View style={{ flex: 3, padding: 5 }}>
               {/* The PanResponder, does not work if there is empty space in the screen */}
-            <Text>
-              Lorem Ipsum is simply dummy text of the printing and typesetting
-              industry. Lorem Ipsum has been the industry's standard dummy text
-              ever since the 1500s, when an unknown printer took a galley of
-              type and scrambled it to make a type specimen book. It has
-              survived not only five centuries, but also the leap into
-              electronic typesetting, remaining essentially unchanged. It was
-              popularised in the 1960s with the release of Letraset sheets
-              containing Lorem Ipsum passages, and more recently with desktop
-              publishing software like Aldus PageMaker including versions of
-              Lorem Ipsum.
-
-              Lorem Ipsum is simply dummy text of the printing and typesetting
-              industry. Lorem Ipsum has been the industry's standard dummy text
-              ever since the 1500s, when an unknown printer took a galley of
-              type and scrambled it to make a type specimen book. It has
-              survived not only five centuries, but also the leap into
-              electronic typesetting, remaining essentially unchanged. It was
-              popularised in the 1960s with the release of Letraset sheets
-              containing Lorem Ipsum passages, and more recently with desktop
-              publishing software like Aldus PageMaker including versions of
-              Lorem Ipsum.
-            </Text>
+              <Text>
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry. Lorem Ipsum has been the industry's standard dummy
+                text ever since the 1500s, when an unknown printer took a galley
+                of type and scrambled it to make a type specimen book. It has
+                survived not only five centuries, but also the leap into
+                electronic typesetting, remaining essentially unchanged. It was
+                popularised in the 1960s with the release of Letraset sheets
+                containing Lorem Ipsum passages, and more recently with desktop
+                publishing software like Aldus PageMaker including versions of
+                Lorem Ipsum. Lorem Ipsum is simply dummy text of the printing
+                and typesetting industry. Lorem Ipsum has been the industry's
+                standard dummy text ever since the 1500s, when an unknown
+                printer took a galley of type and scrambled it to make a type
+                specimen book. It has survived not only five centuries, but also
+                the leap into electronic typesetting, remaining essentially
+                unchanged. It was popularised in the 1960s with the release of
+                Letraset sheets containing Lorem Ipsum passages, and more
+                recently with desktop publishing software like Aldus PageMaker
+                including versions of Lorem Ipsum.
+              </Text>
+            </View>
           </View>
-        </View>
+        </Animated.View>
       );
     });
   };
